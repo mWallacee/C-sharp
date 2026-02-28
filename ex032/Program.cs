e5d347149dcf56db8b8fcf1ec096c45fd7575c49
Wallace <matheuswallace0420@gmail.com>
2026-02-28 11:35:28 -0300

    exercício 32 (Ano bissexto)

---
+++ ex032/Program.cs
@@ -0,0 +1,17 @@
+﻿// Analise de Ano
+
+Console.Write("Digite um ano para analisar (0 = Ano atual): ");
+int AnoEscolhido = int.Parse(Console.ReadLine());
+
+String Ano = "";
+
+if (AnoEscolhido == 0) {
+    AnoEscolhido = DateTime.Now.Year;
+}
+if ((AnoEscolhido  % 4 == 0 && AnoEscolhido % 100 != 0) || AnoEscolhido % 400 == 0){
+    Ano = "É bissexto!!!";
+}
+else{
+    Ano = "Não é bissexto!!! ";
+}
+Console.Write($"O ano {AnoEscolhido} {Ano}");
