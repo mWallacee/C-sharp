3d9c66cfd45dfdf7f9729d8a7da32817f4d79beb
Wallace <matheuswallace0420@gmail.com>
2026-02-28 11:06:11 -0300

    Exercício 31

---
+++ ex031/Program.cs
@@ -0,0 +1,14 @@
+﻿// Agência de viagens
+
+Console.Write("Quantos Kms até o destino: ");
+int kms = int.Parse(Console.ReadLine());
+
+double Passagem = 0;
+
+if (kms > 200){
+    Passagem = kms * 0.45;
+}
+else if (kms < 200){
+    Passagem = kms * 0.5;
+}
+Console.WriteLine($"O valor da passagem ficou no valor de R${Passagem:F2}");
