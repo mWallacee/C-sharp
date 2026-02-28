0122734af58ea28f050191d6a74517cc007db689
Wallace <matheuswallace0420@gmail.com>
2026-02-28 12:22:31 -0300

    Exercício 34 (aumento de salário)

---
+++ ex034/Program.cs
@@ -0,0 +1,14 @@
+﻿// Aumento de Salário
+Console.Write("Qual é o salário atual? R$");
+double Salario = double.Parse(Console.ReadLine());
+
+double Aumento = 0;
+
+if (Salario >= 1250.00){
+    Aumento = Salario + (Salario * 0.10);
+}
+else if (Salario < 1250.00){
+    Aumento = Salario + (Salario * 0.15);
+}
+
+Console.WriteLine($"O aumento de salário foi R${Aumento:F2}");
