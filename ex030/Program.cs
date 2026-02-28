9e84645e3e682efc20585b0cd2528ad7e63194cb
Wallace <matheuswallace0420@gmail.com>
2026-02-28 10:36:48 -0300

    Exercício 30 concluído.

--- ex030/Program.cs
+++ ex030/Program.cs
@@ -3,4 +3,9 @@
 Console.Write("Digite um número: ");
 int Number = int.Parse(Console.ReadLine());
 
-
+if (Number % 2 == 0){
+    Console.WriteLine("Seu número é PAR!!!");
+}
+else{
+    Console.WriteLine("Seu número é ÍMPAR!!!");
+}
