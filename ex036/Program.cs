e215eb9960590b068ab47413e3962d3041862b17
Wallace <matheuswallace0420@gmail.com>
2026-02-28 21:57:16 -0300

    exercício 36 - aprovando empréstimo

---
+++ ex036/Program.cs
@@ -0,0 +1,23 @@
+﻿// Aprovando emprestimo
+
+Console.Write("Informe seu salário R$");
+double salary = double.Parse(Console.ReadLine());
+
+Console.Write("Qual o valor da casa? R$");
+double house = double.Parse(Console.ReadLine());
+
+Console.Write("Em quantos anos irá pagar? ");
+int years = int.Parse(Console.ReadLine());
+
+string Validação = "";
+double Prestação = house / (years * 12);
+
+if (Prestação <= salary  * 0.30){
+     Validação = "Aprovado!!!";
+}
+else if (Prestação >=  salary * 0.30){
+     Validação = "Recusado!!! As parcelas não podem exceder 30% de seu salário.";
+}
+
+Console.WriteLine($"As parcelas ficaram no valor de R${Prestação:F2}");
+Console.WriteLine($"Seu emprestimo foi {Validação}");
