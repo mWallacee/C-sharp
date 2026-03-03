54262b2035357430410aa316d8b10b66389ab713
Wallace <matheuswallace0420@gmail.com>
2026-03-02 22:29:21 -0300

    Fiz um jokenpo em C#

---
+++ ex033/Program.cs
@@ -0,0 +1,41 @@
+﻿// Maior e Menor valor
+
+Console.Write("Primeiro valor: ");
+int Number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Segundo valor: ");
+int Number2 = int.Parse(Console.ReadLine());
+
+Console.Write("Terceiro valor: ");
+int Number3 = int.Parse(Console.ReadLine());
+
+int MaiorValor = 0;
+int MenorValor = 0;
+
+if (Number1 != Number2 && Number3 != Number2 && Number1 != Number3)
+{
+    if (Number1 > Number2 && Number1 > Number3){
+        MaiorValor = Number1;
+    }
+    else{
+        MenorValor = Number1;
+    }
+    if (Number2 >  Number3 && Number2 > Number1){
+        MaiorValor = Number2;
+    }
+    else{
+        MenorValor = Number2;
+    }
+    if (Number3 > Number1 && Number3 > Number2){
+        MaiorValor = Number3;
+    }
+    else{
+        MenorValor = Number3;
+    }
+    Console.WriteLine($"Maior valor: {MaiorValor} \nMenor valor: {MenorValor}");
+}
+else
+{
+    Console.WriteLine("Todos os número são iguais");
+}
+
--- ex045 (Jokenpo)/Program.cs
+++ ex045 (Jokenpo)/Program.cs
@@ -1,3 +1,89 @@
-﻿// Jokenpo
+﻿// Jokenpo v1.0
 
-Console.Write("");
+//await Task.Delay(1000);
+
+//Thread.Sleep(5000);
+
+Console.WriteLine("~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=");
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Jokenpo");
+Console.ResetColor();
+Console.WriteLine("~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=");
+
+string[] itens = { "Pedra", "Papel", "Tesoura" };
+
+Random random = new Random();
+int PcJogada = random.Next(0, 3);
+
+Console.WriteLine("\n-=Opções=-");
+Console.WriteLine("[ 1 ] Pedra");
+Console.WriteLine("[ 2 ] Papel");
+Console.WriteLine("[ 3 ] Tesoura");
+
+Console.Write("Qual será sua jogada? ");
+int PlayerJogada = int.Parse(Console.ReadLine());
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+await Task.Delay(1000);
+Console.WriteLine("\nJO");
+await Task.Delay(1000);
+Console.WriteLine("KEN");
+await Task.Delay(1500);
+Console.WriteLine("PO!!!");
+Console.ResetColor();
+
+Console.WriteLine("\n~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=");
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine($"Computador jogou: {itens[PcJogada]}");
+Console.WriteLine($"Você jogou: {itens[PlayerJogada - 1]}");
+Console.ResetColor();
+Console.WriteLine("~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=\n");
+
+switch (PcJogada)
+{
+    case 0: //pedra
+        if (PlayerJogada == 1) //pedra
+        {
+            Console.WriteLine("EMPATE!!!");
+        }
+        else if (PlayerJogada == 2) //papel
+        {
+            Console.WriteLine("PARABÉNS VOCÊ VENCEU!!!");
+        }
+        else if (PlayerJogada == 3) //tesoura
+        {
+            Console.WriteLine("COMPUTADOR VENCEU!!!");
+        }
+        break;
+
+    case 1: //papel
+        if (PlayerJogada == 1) //pedra
+        {
+            Console.WriteLine("COMPUTADOR VENCEU!!!");
+        }
+        else if (PlayerJogada == 2) //papel
+        {
+            Console.WriteLine("EMPATE!!!");
+        }
+        else if (PlayerJogada == 3) //tesoura
+        {
+            Console.WriteLine("PARABÉNS VOCÊ VENCEU!!!");
+        }
+        break;
+
+    case 2: //tesoura
+        if (PlayerJogada == 1) //pedra
+        {
+            Console.WriteLine("PARABÉNS VOCÊ VENCEU!!!");
+        }
+        else if (PlayerJogada == 2) //papel
+        {
+            Console.WriteLine("COMPUTADOR VENCEU!!!");
+        }
+        else if (PlayerJogada == 3) //tesoura
+        {
+            Console.WriteLine("EMPATE!!!");
+        }
+        break;
+}
+
